1324571c96ad3198e35d88f89cfaae59548a5b7c
Carlos Jordao <carlosjordao@gmail.com>
2024-02-16 12:02:58 -0300

    - refatoramento da build do módulo para python

--- python/setup.py
+++ python/setup.py
@@ -1,39 +1,12 @@
-"""
-   Copyright (C) 2017  Bruno Guberfain do Amaral <bruno.do.amaral@gmail.com>
-
-   Released under the terms of the BSD license
-  
-    Redistribution and use in source and binary forms, with or without modification,
-    are permitted provided that the following conditions are met:
-  
-    1. Redistributions of source code must retain the above copyright notice, this
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, this
-       list of conditions and the following disclaimer in the documentation and/or
-       other materials provided with the distribution.
-  
-  
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  
-  ***********************************************************************
-"""
-from distutils.core import setup, Extension
+from setuptools import Extension, setup
 from os.path import join
 
-c_ext = Extension("metaphoneptbr", ["metaphone_ptbrpy.c", join("..", "source", "metaphone_ptbr.c")])
-
 setup(
-    name='Metaphone-ptbr',
-    version='1.17',
-    ext_modules=[c_ext],
+    name='metaphone-ptbr',
+    version='1.23',
+    ext_modules=[
+        Extension(name="metaphoneptbr", 
+                  sources=["metaphone_ptbrpy.c", join("..", "source", "metaphone_ptbr.c")])
+    ],
     include_dirs=[".", join("..", "source")]
 )
